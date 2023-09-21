dfd2db5182262364219fa31da96e9bfd7ec2ac8b
Yasmany Montiel Rivera <yasmontiel87@gmail.com>
2023-09-21 07:23:47 +0000

    feat(devcontainer feature for develop environment):

---
+++ .devcontainer/dev_config/init_script_commands.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Configuration variables
+
+# Display a message with emoji indicating the configuration is starting
+echo "🚀 Starting configuration..."
+
+echo "🌟 Current directory: $(pwd) "
+
+# Change permissions of the install_oh_my_zsh.sh script
+ sudo chmod a+x .devcontainer/dev_config/install_oh_my_zsh.sh
+
+# Run the install_oh_my_zsh.sh script
+.devcontainer/dev_config/install_oh_my_zsh.sh
+
+echo "⚙️  If needed, run: npm install in a new console to install project npm packages."
+
+npm run start:dev
