0d73594628b81c6b043b1d137a597cee3dc9ba8d
Yasmany Montiel Rivera <yasmontiel87@gmail.com>
2023-09-25 08:34:04 +0000

    Configuration of the migration for the database, creation of the entity classes and relating them to each other

---
+++ src/migrations/1695629994654-init.ts
@@ -0,0 +1,66 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Init1695629994654 implements MigrationInterface {
+    name = 'Init1695629994654'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "public"."users_role_enum" AS ENUM('BASIC', 'ADMIN')`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "first_name" character varying NOT NULL, "last_name" character varying NOT NULL, "age" integer NOT NULL, "email" character varying NOT NULL, "username" character varying NOT NULL, "password" character varying NOT NULL, "role" "public"."users_role_enum" NOT NULL, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE ("username"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TYPE "public"."users_projects_access_level_enum" AS ENUM('40', '50')`);
+        await queryRunner.query(`CREATE TABLE "users_projects" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "access_level" "public"."users_projects_access_level_enum" NOT NULL, "user_id" uuid, "project_id" uuid, CONSTRAINT "PK_f3d2d1a584f1bbc6a91d7ea5773" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "first_name"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "last_name"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "age"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "email"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "username"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "password"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "role"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "first_name" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "last_name" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "age" integer NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "email" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email")`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "username" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE ("username")`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "password" character varying NOT NULL`);
+        await queryRunner.query(`CREATE TYPE "public"."users_role_enum" AS ENUM('BASIC', 'ADMIN')`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "role" "public"."users_role_enum" NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "name" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "description" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users_projects" ADD CONSTRAINT "FK_0f280c70a3a6ab7f4cf3c658c4c" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "users_projects" ADD CONSTRAINT "FK_741210c246defe00ed877a98f2a" FOREIGN KEY ("project_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "users_projects" DROP CONSTRAINT "FK_741210c246defe00ed877a98f2a"`);
+        await queryRunner.query(`ALTER TABLE "users_projects" DROP CONSTRAINT "FK_0f280c70a3a6ab7f4cf3c658c4c"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "description"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "name"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "role"`);
+        await queryRunner.query(`DROP TYPE "public"."users_role_enum"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "password"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "username"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "email"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "age"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "last_name"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "first_name"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "role" "public"."users_role_enum" NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "password" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "username" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE ("username")`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "email" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email")`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "age" integer NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "last_name" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "first_name" character varying NOT NULL`);
+        await queryRunner.query(`DROP TABLE "users_projects"`);
+        await queryRunner.query(`DROP TYPE "public"."users_projects_access_level_enum"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+        await queryRunner.query(`DROP TYPE "public"."users_role_enum"`);
+    }
+
+}
