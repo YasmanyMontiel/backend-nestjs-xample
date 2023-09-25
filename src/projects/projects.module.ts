b7c4a17a6e37753dd13dbe412fb16b0d92bfca4f
Yasmany Montiel Rivera <yasmontiel87@gmail.com>
2023-09-25 12:32:45 +0000

    deleting migration file

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './services/projects.service';
 import { ProjectsController } from './controllers/projects.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProjectsEntity } from './entities/projects.entity';
-import { UsersProjectsEntity } from 'src/users/entities/usersProjects.entity';
-import { UsersService } from 'src/users/services/users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProjectsEntity, UsersProjectsEntity])],
-  providers: [ProjectsService, UsersService],
+  providers: [ProjectsService],
   controllers: [ProjectsController],
 })
 export class ProjectsModule {}
