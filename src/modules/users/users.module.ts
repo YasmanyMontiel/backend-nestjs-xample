8dbc077b1c18f7a5f6d28d9cf73db0c5b02b490a
Yasmany Montiel Rivera <yasmontiel87@gmail.com>
2023-09-27 08:47:56 +0000

    export UserDtoValidator

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,11 +4,12 @@ import { UsersController } from './controllers/users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersEntity } from './entities/users.entity';
 import { UsersProjectsEntity } from './entities/usersProjects.entity';
+import { UserDtoValidator } from './validator/user.validator';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UsersEntity, UsersProjectsEntity])],
-  providers: [UsersService],
+  providers: [UsersService, UserDtoValidator],
   controllers: [UsersController],
-  exports: [UsersService, TypeOrmModule],
+  exports: [UsersService, TypeOrmModule, UserDtoValidator],
 })
 export class UsersModule {}
